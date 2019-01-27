58e80aa81c58a95cbd7bedcacae726fe7652467a
omerb304 <omerbachartadam@gmail.com>
2019-01-27 18:22:25 +0200

    Settings.ini added + updates

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import re
 import json
 import requests
 import time
+import configparser
 
 """
  Checks if given string is a valid url
- @param String str - given string
- @return Boolean
+ @param {String} str - given string
+ @return {Boolean}
 """
+
+
 def is_url(checked_string):
     regex = re.compile(
         r'^(?:http|ftp)s?://'  # http:// or https://
@@ -20,34 +23,61 @@ def is_url(checked_string):
         r'(?::\d+)?'  # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-    return (re.match(regex, checked_string) is not None);
+    return (re.match(regex, checked_string) is not None)
+
 
 """
  Checks if given url is mobile friendly
- @param String site_url - given url
- @return Boolean
+ @param {String} site_url - given url
+ @return {Boolean}
 """
+
+
 def is_url_friendly(site_url):
-    url = "https://www.googleapis.com/pagespeedonline/v3beta1/mobileReady?url=%s" % site_url;
+    url = "https://www.googleapis.com/pagespeedonline/v3beta1/mobileReady?url=%s" % site_url
     data = requests.get(url).text
     data = json.loads(data)
 
     if('error' in data):
-        print("Test for '%s' has an error: %s" % (site_url,data['error']['errors'][0]['message']))
-        return False;
+        print("Test for '%s' has an error: %s" %
+              (site_url, data['error']['errors'][0]['message']))
+        return False
     if('ruleGroups' in data):
         message = None
-        if( data['ruleGroups']['USABILITY']['pass'] == True):
+        if(data['ruleGroups']['USABILITY']['pass'] == True):
             message = "Success"
         else:
             message = "Failed"
-        print("%s - %s" % (site_url,message))
+        print("%s - %s" % (site_url, message))
         return (data['ruleGroups']['USABILITY']['pass'])
     return False
 
+
+"""
+Get (and parse) settings from config.ini
+@return {Object}
+"""
+
+
+def getSettings():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    arr = {}
+    arr['delay'] = config.get('Settings', 'Delay')
+    arr['filePath'] = config.get('Settings', 'FilePath')
+
+    return arr
+
+
 def __main__():
+
+    # get settings from config file
+    settings = getSettings()
+
     # check for urls.txt
-    if not os.path.isfile("urls.txt"):
+    print(settings)
+    if not os.path.isfile(settings['filePath']):
         print("urls.txt is missing")
         return
 
@@ -55,18 +85,18 @@ def __main__():
     with open("urls.txt", "r") as f:
         arr = []
         for line in f:
-            line = line.replace("\n","");
+            line = line.replace("\n", "")
             if(is_url(line)):
                 print(line)
                 arr.append(line)
             else:
                 print("%s is not a valid url" % line)
-    
+
     # check if urls were found
     if(len(arr) == 0):
-        print("No valid urls were found on urls.txt");
-        return;
-    
+        print("No valid urls were found on urls.txt")
+        return
+
     # DO THE CHECk
     failed = []
 
@@ -74,12 +104,14 @@ def __main__():
         if(not is_url_friendly(url)):
             failed.append(url)
         # Sleep for 2 seconds
-        time.sleep(2)
+        time.sleep(int(settings['delay']))
 
-    print("%s urls are failed in the mobile friendly test." % len(failed))
-    print(failed)
-    
-        
+    if(len(failed) == 0):
+        print("The whole urls has passed the test!")
+    else:
+        print("%s urls are failed in the mobile friendly test." % len(failed))
+        print(failed)
+
 
 if(__name__ == "__main__"):
     __main__()
